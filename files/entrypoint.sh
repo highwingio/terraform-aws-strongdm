46d73b8bd86f9c06d2335b05c2884777a73ea067
Tim Hartmann <tfhartmann@gmail.com>
2018-05-14 11:07:51 -0400

    Updated entrypoint script

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
 
-sdm status >/dev/null;
-sdm relay create > /tmp/key
-SDM_RELAY_TOKEN=$(cat /tmp/key)
-export SDM_RELAY_TOKEN
-sdm relay
-#echo $SDM_RELAY_TOKEN
+CMD=/sdm.linux
+
+# necessary to suppress stdout during token create
+unset SDM_DOCKERIZED
+
+# generate fresh relay token (depends on inheriting SDM_ADMIN_TOKEN)
+export SDM_RELAY_TOKEN=`$CMD relay create`
+
+# temporary auth state is created by invoking `relay create` and must
+# be cleared out prior to relay startup
+rm /root/.sdm/*
+unset SDM_ADMIN_TOKEN
+
+# --daemon arg automatically respawns child relay process during
+# version upgrades or abnormal termination
+export SDM_DOCKERIZED=true # reinstate stdout logging
+$CMD relay --daemon
