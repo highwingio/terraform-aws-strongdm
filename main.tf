0893411d43e51d84ff93c759f86e4819756155d6
Jay Zeschin <jay.zeschin@highwing.io>
2021-10-21 16:49:26 -0600

    Convert the listen port to a string when putting it in an environment variable

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ locals {
     },
     {
       "name"  = "SDM_GATEWAY_LISTEN_APP_PORT"
-      "value" = var.sdm_gateway_listen_app_port
+      "value" = tostring(var.sdm_gateway_listen_app_port)
     },
     {
       "name"  = "PUBLIC_HOST"
