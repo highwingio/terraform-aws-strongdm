d440ca0dc1e3d24d0be789d38c7b3ebecff5c8be
Jay Zeschin <jay@zeschin.org>
2023-01-20 15:57:31 -0700

    Set a name for the SDM node using service_identifier

--- sdm.tf
+++ sdm.tf
@@ -1,5 +1,6 @@
 resource "sdm_node" "gateway" {
   gateway {
+    name = var.service_identifier
     listen_address = "${aws_lb.nlb.dns_name}:${var.gateway_listen_port}"
     bind_address   = "0.0.0.0:${var.gateway_listen_port}"
   }
